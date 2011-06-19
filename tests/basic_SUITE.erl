a82de33689dc707bb36c724e29c1f8112b281402
Lukas Larsson <garazdawi@gmail.com>
2011-06-19 21:23:28 +0200

    Extend basic tests to create a small function

--- tests/basic_SUITE.erl
+++ tests/basic_SUITE.erl
@@ -21,15 +21,20 @@ basic(_Config) ->
     Ctx = llevm:'LLVMGetGlobalContext'(),
     ModRef = llevm:'LLVMModuleCreateWithName'("test"),
     FunTypeRef = llevm:'LLVMFunctionType'(llevm:'LLVMDoubleType'(),
-				       {llevm:'LLVMDoubleType'(),
-				        llevm:'LLVMDoubleType'()}, 2, false),
+					  {llevm:'LLVMDoubleType'(),
+					   llevm:'LLVMDoubleType'() }, 2, false),
     FunRef = llevm:'LLVMAddFunction'(ModRef, "foo", FunTypeRef),
     llevm:'LLVMDumpModule'(ModRef),
     BuildRef = llevm:'LLVMCreateBuilderInContext'(Ctx),
-    _BB = llevm:'LLVMAppendBasicBlockInContext'(Ctx,FunRef,"entry"),
-    Value = llevm:'LLVMConstReal'(llevm:'LLVMDoubleType'(),
-				  1.2),
-    llevm:'LLVMBuildRet'(BuildRef, Value),
+    BB = llevm:'LLVMAppendBasicBlock'(FunRef,"entry"),
+    llevm:'LLVMPositionBuilderAtEnd'(BuildRef, BB),
+    Value10 = llevm:'LLVMConstReal'(llevm:'LLVMDoubleType'(),
+				    1.0),
+    ValueArg1 = llevm:'LLVMGetParam'(FunRef, 0),
+    ValueArg2 = llevm:'LLVMGetParam'(FunRef, 1),
+    
+    ValueAdd = llevm:'LLVMBuildFAdd'(BuildRef, Value10, ValueArg1, "addtmp"),
+    ValueMul = llevm:'LLVMBuildFMul'(BuildRef, ValueAdd, ValueArg2, "multmp"),
+    llevm:'LLVMBuildRet'(BuildRef, ValueMul),
     
     llevm:'LLVMDumpModule'(ModRef).
-	
